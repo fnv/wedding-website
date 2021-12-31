24b467513e14e296bf3edb4cf0110af70c5d8007
Ted Nichols <foster.nichols@gmail.com>
2021-12-31 17:18:12 -0500

    update to latest deploy

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyeFex2fc40sSgrQvB7BZn2Ce91LAErYefqXoF_8cJE7APxglEprOHKhbzq6i0tpxZZ/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxrqAYpuWaMgaRCrDQw-0HllKs8EdTx1z9yGKp8Vr6z9XpAA4d4BwkN22FnBp5qcg3C/exec, data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
